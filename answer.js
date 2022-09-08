4ff48ea9c71fdf7bf88a2cecb93fd7ecf5a27fab
mnubhan <76425959+mnubhan@users.noreply.github.com>
2022-09-09 00:49:34 +0800

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,23 @@
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+  
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  }
+  
+  return dt === 10 && dx === 0 && dy === 0
+}
+
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}
